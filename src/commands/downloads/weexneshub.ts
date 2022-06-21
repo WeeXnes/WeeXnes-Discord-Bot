82678de8cc1f9813c52efffe013b0564c9ee7d6f
WeeXnes <somtrigi@gmail.com>
2022-06-21 19:57:47 +0200

    automatic api update

--- src/commands/downloads/weexneshub.ts
+++ src/commands/downloads/weexneshub.ts
@@ -5,14 +5,29 @@ import fetch from "node-fetch";
 
 
 
+
 export default new Command({
     name: 'weexnessuite',
     description: 'Sends you the link to the WeexnesSuite Download',
     run: async ({interaction}) => {
+        let version:String;
+        let downloadUrl:String;
+        fetch("http://www.weexnes.com:5169/", {
+            method: 'GET'
+        }).then(res => res.json())
+            .then(json => {
+                console.log(json.tag_name)
+                version = json.tag_name;
+                downloadUrl = json.download_url;
+                interaction.followUp("Current Version: " + version + " | Check your DMs!");
+                interaction.member.createDM().then(channel => {
+                    channel.send("Here's your download: " + downloadUrl);
+                });
+            })
+            .catch(err => {
+                console.log(err)
+                interaction.followUp("An error has occured!");
+            });
 
-        interaction.followUp("Current Version: " + config.weexnessuite.currentVersion + " | Check your DMs!");
-        interaction.member.createDM().then(channel => {
-            channel.send("Here's your download: " + config.weexnessuite.downloadUrl);
-        });
     }
 });
